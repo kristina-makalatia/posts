04273eb4d1bfc2567c5b23e6a54eede5015a5191
kristina-makalatia <kristina.makalatia@geolab.edu.ge>
2022-06-04 19:21:22 +0400

    add final

--- script.js
+++ script.js
@@ -1,4 +1,160 @@
 
+// json.parse() - string -> Object
+// json.stringify() - object -> string
+
+// posts practice
+
+let mainWrapper = document.getElementById('post-block-wraper');
+let overlay = document.getElementById('overlay-post');
+let content = document.getElementById('content');
+let closeOverlay = document.getElementById('closeOverlay');
+let addPost = document.getElementById('add');
+let postOverlay = document.getElementById('postoverlay');
+let form = document.getElementById('form');
+
+function ajax(url,callback) {
+    let requist = new XMLHttpRequest();
+    requist.open('GET', url);
+    requist.addEventListener('load', function() {
+        // let data = requist.responseText;
+        // let responseDataPosts = JSON.parse(data);
+        let data = JSON.parse(requist.responseText);
+        callback(data);
+    });
+    requist.send();
+}
+
+ajax('https://jsonplaceholder.typicode.com/posts', function(data) {
+    printData(data);
+});
+
+function printData(data) {
+    data.forEach(element => {
+        createPost(element);
+    })
+}
+
+//ფუნქცია რომლის საშუალებით შევქმნი პოსტს
+function createPost(item) {
+    let divWrapper = document.createElement('div');
+    divWrapper.classList.add('posts');
+    divWrapper.setAttribute('data-id', item.id);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('data-id', item.id);
+    deleteButton.innerText = 'Delete This Post';
+
+    let h3Tag = document.createElement('h3');
+    h3Tag.innerText = item.id;
+
+    let h2Tag = document.createElement('h2');
+    h2Tag.innerText = item.title;
+
+    divWrapper.appendChild(h3Tag);
+    divWrapper.appendChild(h2Tag);
+    divWrapper.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        let id = event.target.getAttribute('data-id');
+        deletePost(id);
+    });
+
+
+    // კონკრეტული პოსტის დაჭერის დროს ამოვაგდოთ პოპაპი
+    divWrapper.addEventListener('click', function(event) {
+        let id = event.target.getAttribute('data-id');
+        openOverlay(id);
+    })
+
+    mainWrapper.appendChild(divWrapper);
+
+    console.log(divWrapper);
+}
+
+
+// ფუნქციის საშუალებით ამოვაგდებთ პოპაპს
+function openOverlay(id) {
+    overlay.classList.add('active');
+    let url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    ajax(url, function(data) {
+        overlayFunction(data);
+    })
+    console.log(id);
+}
+
+// პოსტის წაშლის ფუნქცია
+function deletePost(id) {
+    let url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    fetch(url, {
+        method: 'DELETE',
+    })
+    .then(response => response.json() )
+    .then(data => {
+        console.log(data);
+    })
+}
+
+
+function overlayFunction(item) {
+    let spanUserId = document.createElement('span');
+    spanUserId.innerText = item.userId;
+
+    let descriptionPost = document.createElement('p');
+    descriptionPost.innerText = item.body;
+
+    content.innerHTML = '';
+    content.appendChild(spanUserId);
+    content.appendChild(descriptionPost);
+
+}
+// დავხურავთ პოპაპს
+closeOverlay.addEventListener('click', function() {
+    overlay.classList.remove('active');
+    // content.innerHTML = '';
+});
+
+
+// პოსტის დამატება
+addPost.addEventListener('click', function() {
+    postOverlay.classList.add('active');
+})
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let formData = {
+        title: event.target[0].value,
+        body: event.target[1].value
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+            postOverlay.classList.remove('active');
+        });
+
+
+    console.log(formData);
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
 //load more example
 let currentPage = 1;
 let totalPages;
@@ -71,77 +227,3 @@ document.getElementById('loadnext').addEventListener('click', function() {
 })
 
 getUsers(currentPage);
-
-
-// json.parse() - string -> Object
-// json.stringify() - object -> string
-
-// posts practice
-
-let mainWrapper = document.getElementById('post-block-wraper');
-let overlay = document.getElementById('overlay-post');
-let closeOverlay = document.getElementById('closeOverlay');
-
-
-
-function ajax() {
-    let requist = new XMLHttpRequest();
-    requist.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-    requist.addEventListener('load', function() {
-        // let data = requist.responseText;
-        // let responseDataPosts = JSON.parse(data);
-
-        let data = JSON.parse(requist.responseText);
-
-        data.forEach(element => {
-            createPost(element);
-        })
-
-
-        console.log(data);
-    });
-    requist.send();
-}
-
-
-//ფუნქცია რომლის საშუალებით შევქმნი პოსტს
-function createPost(item) {
-    let divWrapper = document.createElement('div');
-    divWrapper.classList.add('posts');
-    divWrapper.setAttribute('data-id', item.id);
-
-    let h3Tag = document.createElement('h3');
-    h3Tag.innerText = item.id;
-
-    let h2Tag = document.createElement('h2');
-    h2Tag.innerText = item.title;
-
-    divWrapper.appendChild(h3Tag);
-    divWrapper.appendChild(h2Tag);
-
-
-    // კონკრეტული პოსტის დაჭერის დროს ამოვაგდოთ პოპაპი
-    divWrapper.addEventListener('click', function(event) {
-        let id = event.target.getAttribute('data-id');
-        openOverlay(id);
-    })
-
-    mainWrapper.appendChild(divWrapper);
-
-    console.log(divWrapper);
-}
-
-
-// ფუნქციის საშუალებით ამოვაგდებთ პოპაპს
-function openOverlay(id) {
-    overlay.classList.add('active');
-    console.log(id);
-}
-
-// დავხურავთ პოპაპს
-closeOverlay.addEventListener('click', function() {
-    overlay.classList.remove('active');
-});
-
-
-ajax();
